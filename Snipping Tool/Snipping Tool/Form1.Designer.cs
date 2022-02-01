3cc2656b00d2a7a59066fc9b61145bc2c3eb0d57
Arda Gülez <51272898+devRaikou@users.noreply.github.com>
2022-02-01 13:20:47 +0300

    Add files via upload

---
+++ Snipping Tool/Snipping Tool/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Snipping_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 223);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(554, 396);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Snipping Tool/Snipping Tool/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snipping_Tool
+{
+    public partial class Form1 : Form
+    {
+        int selectX;
+        int selectY;
+        int selectWidth;
+        int selectHeight;
+        public Pen selectPen;
+
+        bool start = false;
+        private void SaveToClipBoard()
+        {
+            if(selectWidth > 0)
+            {
+                Rectangle rect = new Rectangle(selectX, selectY, selectWidth, selectHeight);
+                Bitmap OriginalImage = new Bitmap(pictureBox1.Image, pictureBox1.Width, pictureBox1.Height);
+                Bitmap _img = new Bitmap(selectWidth, selectHeight);
+                Graphics g = Graphics.FromImage(_img);
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.DrawImage(OriginalImage,0,0,rect,GraphicsUnit.Pixel);
+                Clipboard.SetImage(_img);
+            }
+            Application.Exit();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            Bitmap printScreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printScreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printScreen.Size);
+            using (MemoryStream s = new MemoryStream())
+            {
+                printScreen.Save(s, ImageFormat.Bmp);
+                pictureBox1.Size = new Size(this.Width, this.Height);
+                pictureBox1.Image = Image.FromStream(s);
+            }
+            this.Show();
+            Cursor = Cursors.Cross;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(pictureBox1.Image == null)
+            {
+                return;
+            }
+            if (start)
+            {
+                pictureBox1.Refresh();
+                selectWidth = e.X - selectX;
+                selectHeight = e.Y - selectY;
+                pictureBox1.CreateGraphics().DrawRectangle(selectPen,selectX,selectY,selectWidth, selectHeight);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!start)
+            {
+                
+                if(e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    selectX = e.X;
+                    selectY = e.Y;
+                    selectPen = new Pen(Color.Red, 1);
+                    selectPen.DashStyle = DashStyle.DashDotDot;
+
+                }
+                pictureBox1.Refresh();
+                start = true;
+            }
+            else
+            {
+                if(pictureBox1.Image == null)
+                    return;
+                if(e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    pictureBox1.Refresh();
+                    selectWidth = e.X - selectX;
+                    selectHeight = e.Y - selectY;
+                    pictureBox1.CreateGraphics().DrawRectangle(selectPen, selectX, selectY, selectWidth, selectHeight);
+                }
+                start = false;
+                SaveToClipBoard();
+            }
+        }
+    }
+}
